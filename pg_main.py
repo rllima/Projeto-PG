aabd3b3e034cd73553670ef35e5f66bed66b7327
rllima <rlo@cin.ufpe.br>
2019-05-13 20:57:15 -0300

    Correction in Normalize function, intersect function test

--- pg_main.py
+++ pg_main.py
@@ -11,7 +11,7 @@ class Vector:
     def norm(self):
         return np.linalg.norm(self)
     def normalize(self):
-        return self /self.norm()
+        return Vector(self.x /self.norm(),self.y/self.norm(), self.z/self.norm())
     def add(self, other):
         return np.add(self, other)
     def sub(self, other):
@@ -36,5 +36,15 @@ class Sphere:
             então devemos retornar a distancia entre o ponto de intercção e os olhos, caso
             contrario retornamos None
         """
+
+        ray_to_sphere = ray.origin - self.origin #Representa a distancia entre o inicio do raio e a posição do circulo
+        b = 2* np.dot(ray.direction,ray_to_sphere)
+        c = ray_to_sphere ** 2 - self.radius ** 2
+        discriminant = b ** 2 - 4 * c
+
+        if discriminant >= 0:
+            dist = (-b - math.sqrt(discriminant))/2
+            if dist > 0:
+                return dist 
 
         
