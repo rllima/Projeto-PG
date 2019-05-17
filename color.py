2dcca3c929bf3502bc54e501bb62bf1ca32194ee
Rielson-Leandro <rlsl@cin.ufpe.br>
2019-05-17 14:20:49 -0300

    modificando cor

--- color.py
+++ color.py
@@ -1,19 +1,20 @@
-class Color():
+class Color(object):
+    #Vai descrver as cores dos objetos
+
     def __init__(self, r, g, b):
-        self.r = r
-        self.g = g
-        self.b = b
+        #O construtor verifica se os componentes dados então entre 0 e 255
+        self.r = max(min(r, 255), 0)
+        self.g = max(min(g, 255), 0)
+        self.b = max(min(b, 255), 0)
     
-    def __add__(self, other):
-        #Operador +
-        if type(other).__name__=='Color':
-            return Color(self.r+other, self.g+other.g, self.b+other.b)
-        else:
-            return Color(self.r+other, self.g+other, self.b+other)
+    def __add__(self, c):
+        #Adiciona duas cores
+        return Color(min(self.r + c.r, 255), min(self.g + c.g, 255), min(self.b + c.b, 255))
     
     def __mult__(self, f):
-        #Operador *
-        if type(other).__name__ == 'Color':
-            return Color(self.r*other.r, self.g*other.g, self.b*other.b)
-        else:
-            return Color(self.r*other, self.g*other, self.b*other)
+        #Multiplica cada componente de cor com um determinado fator
+
+        r = max(min(self.r * f, 255), 0)
+        g = max(min(self.g * f, 255), 0)
+        b = max(min(self.b * f, 255), 0)
+        return Color(r, g, b)
