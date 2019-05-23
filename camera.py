cf9307bca17a5a867a4f6d697f57463b9ad465c0
affsvelho <affs@cin.ufpe.br>
2019-05-23 00:44:22 -0300

    added new comments in the camera file

--- camera.py
+++ camera.py
@@ -6,11 +6,11 @@ from vector import *
 
 class Camera(object):
     #Essa classe descreve a camera
-    #fov = fieldOfView
-    #pos = posição
-    #target = Ponto de mira
-    #up vetor que aponta direção
-    #dist = distancia
+    #fov = fieldOfView (angulo entre o tamanho da porta de visualização e a distancia até a camera)
+    #pos = posição do "olho" no espaço (posicao da camera) - ponto
+    #target = Ponto de mira (ponto da camera) - ponto
+    #up - direção perpendicular a direção em q o "olho" ta apontando - vetor
+    #dist = distancia do "olho" até o plano
     #viewWidth = largura da imagem
     #viewheigth = Altura da imagem
 
@@ -23,12 +23,12 @@ class Camera(object):
         self.viewheigth = float(viewheigth)
         self.viewWidth = float(viewWidth)
 
-        self.alpha = (fov / 180 * pi)/2
-        self.height = 2 * tan(self.alpha)
+        self.alpha = (fov / 180 * pi)/2 
+        self.height = 2 * tan(self.alpha) #angulo de abertura da camera
         self.width = (self.viewWidth/self.viewheigth) * self.height
-        self.f = (self.target - self.pos).norm() #O vetor vai para o centro
-        self.s = (self.f.cross(self.up)).norm() #Eixo X do vetor
-        self.u = self.s.cross(self.f) # Eixo Y do vetor
+        self.f = (self.target - self.pos).norm() #O vetor vai para o centro/ vetor de direção da camera (precisa inverter??)
+        self.s = (self.f.cross(self.up)).norm() #Eixo X do vetor positivo do espaço da camera
+        self.u = self.s.cross(self.f) # Eixo Y do vetor 
 
     def CalcRay(self, x, y):
         #Calcula o raio dependendo dos paramentros da camera. x e y. pixels
--- ray.py
+++ ray.py
@@ -4,11 +4,12 @@ from vector import *
 class Ray(object):
 	#Esta classe representa um raio com uma origem e uma direção
 	def __init__(self, origin, direction):
-		self.origin = origin #ponto
-		self.direction = direction.norm() #vetor
+		self.origin = origin #ponto de origem do raio 
+		self.direction = direction.norm() #vetor direção desse ponto, normalizado
 	
 	def pointParametro(self, t):
-		#Retorna um ponto neste raio em um determinado parametro t
+		#Retorna um ponto neste raio em um determinado parametro t, matematicamente expressa qualquer ponto nessa linnha
+		#equação parametrica
 		return self.origin + self.direction.scale(t)
 
 	def __rep__(self):
@@ -16,6 +17,7 @@ class Ray(object):
 		return 'Ray(%s, %s)' %(repr(self.origin), repr(self.direction))
 
 	def point_at_dist(self, dist):
+		#distancia para a superficie visivel
 		return self.origin + self.direction * dist
 	
 	def invert(self):
