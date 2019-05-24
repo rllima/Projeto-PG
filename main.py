f4fd41ec439d12d779c96d69ef39bb25e231c4be
Rielson-Leandro <rlsl@cin.ufpe.br>
2019-05-24 01:03:43 -0300

    adicionando leitura de arquivo

--- main.py
+++ main.py
@@ -8,6 +8,54 @@ from light import Light
 from plane import Plane
 
 if __name__=="__main__":
+
+	c_pos = {'x':0.0, 'y':0.0, 'z':0.0}
+	c_target = {'x':0.0,'y':0.0,'z':0.0}
+	f = {'x':0.0, 'y':0.0, 'z':0.0}
+	fov = {'x':0.0, 'y':0.0, 'z':0.0}
+	up = {'x':0.0, 'y':0.0, 'z':0.0}
+	height = 0.0
+	width = 0.0
+
+	def getTxt():
+		data = open("input/entrada.txt","r")
+		inputs = data.readlines()
+
+		global c_pos,c_target,f,fov,up,height,width
+
+		height = inputs[3].split(' ')[0]
+		width = inputs[3].split(' ')[1]
+
+		C_pos_in = inputs[0].replace('\n','').replace('\r','')
+		C_target_in = inputs[1].replace('\n','').replace('\r','')
+		C_f_in = inputs[2].replace('\n','').replace('\r','')
+		C_fov_in = inputs[3].replace('\n','').replace('\r','')
+		C_up_in = inputs[4].replace('\n','').replace('\r','')
+		C_height_in = inputs[5].replace('\n','').replace('\r','')
+		C_width_in = inputs[6].replace('\n','').replace('\r','')
+
+		c_pos['x'] = C_pos_in.split(' ')[0]
+		c_pos['y'] = C_pos_in.split(' ')[1]
+		c_pos['z'] = C_pos_in.split(' ')[2]
+
+		c_target['x'] = C_target_in.split(' ')[0]
+		c_target['y'] = C_target_in.split(' ')[0]
+		c_target['z'] = C_target_in.split(' ')[0]
+
+		f['x'] = C_f_in.split(' ')[0]
+		f['y'] = C_f_in.split(' ')[1]
+		f['z'] = C_f_in.split(' ')[2]
+
+		fov['x'] = C_fov_in.split(' ')[0]
+		fov['y'] = C_fov_in.split(' ')[1]
+		fov['z'] = C_fov_in.split(' ')[2]
+
+		up['x'] = C_up_in.split(' ')[0]
+		up['y'] = C_up_in.split(' ')[1]
+		up['z'] = C_up_in.split(' ')[2]
+		
+
+
 	Point = Vector
 	Color = Vector
 	objects = [
